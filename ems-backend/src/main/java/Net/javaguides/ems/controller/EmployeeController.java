ade80f4d8ee66551383cb08685679abb085b9dee
Avinash <himachandrakonakalla@gmail.com>
2025-12-18 03:04:03 -0500

    Add GET endpoint to fetch employee by ID
    - Added @GetMapping("/{id}") method in EmployeeController
    - Uses @PathVariable to extract employee ID from URL
    - Calls employeeService.getEmployeeById(id)
    - Returns employee data with 200 OK status

---
+++ ems-backend/src/main/java/Net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package Net.javaguides.ems.controller;
+
+import Net.javaguides.ems.dto.EmployeeDto;
+import Net.javaguides.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // GET all employees
+    @GetMapping
+    public ResponseEntity<java.util.List<EmployeeDto>> getAllEmployees() {
+        java.util.List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    // GET employee by ID - NEW FEATURE WE'RE ADDING
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id) {
+        EmployeeDto employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    // POST - Create new employee
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
